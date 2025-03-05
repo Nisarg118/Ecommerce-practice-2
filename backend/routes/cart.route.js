9e005dfd30df2a0b3acdd15cfc5a4ec95da84cdd
Nisarg118 <nisargbhamat12@gmail.com>
2025-03-05 10:24:04 +0530

    Cart Routes and CRUD

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addToCart,
+  deleteAllFromCart,
+  getCartProducts,
+  updateQuantity,
+} from "../controllers/cart.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, deleteAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
+export default router;
